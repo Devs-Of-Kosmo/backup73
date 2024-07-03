0f9c5ab67d0cfa6f6a8d17bb009505fc0dfa631a
dongkyukim1 <wlsntus55@gmail.com>
2024-07-03 12:01:52 +0900

    Add files via upload

---
+++ devhub-server/src/main/java/team/devs/devhub/DevhubApplication.java
@@ -0,0 +1,14 @@
+package team.devs.devhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class DevhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevhubApplication.class, args);
+	}
+
+}
